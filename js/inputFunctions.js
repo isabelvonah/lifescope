b2c25bd487bcabbfee287fc60062a773bf02a8c7
isabelvonah <isabelvonah@gmail.com>
2021-10-20 17:19:50 +0200

    add constructWaffle()

--- js/inputFunctions.js
+++ js/inputFunctions.js
@@ -1,3 +1,19 @@
+function constructWaffle() {
+    age = document.getElementById("age").value;
+
+    if (age < 75) {
+
+        dataset.push({category: "lived", count: parseInt(age) * 52});
+
+        //https://www.worldometers.info/demographics/life-expectancy/
+        dataset.push({category: "to live", count: (75 - parseInt(age)) * 52});
+
+        DotMatrixChart( dataset, chart_options );
+    }
+
+    // TODO: else {print "Congratulations, your reached a very nice age."} 
+}
+
 function addFiftyDots() {
     dataset[dataset.length -1].count += 500;
     DotMatrixChart( dataset, chart_options );
