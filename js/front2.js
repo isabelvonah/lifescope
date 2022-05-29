9c88b33d6059908e05924006a483eaf5c80ca56f
Isabel von Ah <isabelvonah@gmail.com>
2022-05-29 16:48:49 +0200

    prepare restorePerson()

--- js/front2.js
+++ js/front2.js
@@ -19,7 +19,48 @@ function createDataObject() {
     
 }
 
+let testPerson = {
+    id: "aösdkfjaösdfjk",
+    nickname: "test",
+    age: "25",
+    sex: "female",
+    data: {
+        sleep: "6",
+        work: "30",
+        ageOfRetirement: "70",
+        media: "0.5",
+        admin: "0.5"
+    }
+}
+
 function restorePerson() {
-    console.log("restored");
+
+    // check if id (parameter) exists
+    // ...
+    // yes: replace testPerson
+    // no: throw error-msg
+
+    document.getElementById("nickname").value = testPerson.nickname;
+    document.getElementById("age").value = testPerson.age;
+    
+    if (testPerson.sex == "female") {
+        document.getElementById("female").classList.add('selected');
+        document.getElementById("male").classList.remove('selected');
+        setSex("female");
+    } else if (testPerson.sex == "male") {
+        document.getElementById("male").classList.add('selected');
+        document.getElementById("female").classList.remove('selected');
+        setSex("male")
+    } else {
+        document.getElementById("male").classList.remove('selected');
+        document.getElementById("female").classList.remove('selected');
+    }
+
+    document.getElementById("sleep").value = testPerson.data.sleep;
+    document.getElementById("work").value = testPerson.data.work;
+    document.getElementById("ageOfRetirement").value = testPerson.data.ageOfRetirement;
+    document.getElementById("media").value = testPerson.data.media;
+    document.getElementById("admin").value = testPerson.data.admin
+
 }
 
